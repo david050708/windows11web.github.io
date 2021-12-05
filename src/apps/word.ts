990e19c5d708ba994491ccc753af313211019620
david050708 <85152515+david050708@users.noreply.github.com>
2021-12-05 12:50:21 +0530

    Add files via upload

---
+++ src/apps/word.ts
@@ -0,0 +1,10 @@
+import Window from "../components/window/window";
+import createElement from "../createElement";
+import { IWindow } from "../interfaces/window";
+import { getFileName, readFile } from "../utils";
+import { IAppInfo } from "./appInfo";
+
+
+<iframe src="https://www.office.com/launch/word?auth=2">
+</iframe>
+export default Word
